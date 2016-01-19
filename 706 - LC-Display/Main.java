6ea5bd1bb6e04e729720560398d7505a7aca95bf
kolotaev <kolotaev@tut.by>
2016-01-19 14:37:29 +0300

    LCD new solution accepted

--- 706 - LC-Display/Main.java
+++ 706 - LC-Display/Main.java
@@ -3,6 +3,19 @@ import java.util.*;
 
 public class Main {
     private PrintStream o;
+    // d -> '-'; s -> ''; r -> '| '; l -> ' |'; w -> '| |'
+    private static char[][] templates = {
+            {'d', 'w', 's', 'w', 'd'}, // 0
+            {'s', 'r', 's', 'r', 's'}, // 1
+            {'d', 'r', 'd', 'l', 'd'}, // 2
+            {'d', 'r', 'd', 'r', 'd'}, // 3
+            {'s', 'w', 'd', 'r', 's'}, // 4
+            {'d', 'l', 'd', 'r', 'd'}, // 5
+            {'d', 'l', 'd', 'w', 'd'}, // 6
+            {'d', 'r', 's', 'r', 's'}, // 7
+            {'d', 'w', 'd', 'w', 'd'}, // 8
+            {'d', 'w', 'd', 'r', 'd'}, // 9
+    };
 
     public static void main(String[] args) {
         try {
@@ -31,51 +44,28 @@ public class Main {
     public void displayLCD(int scale, int number) {
         char[] digits = Integer.toString(number).toCharArray();
 
-        if  (scale > 0) {
-            printRow("top", digits, scale);
-        }
-
-        for (int i = 1; i <= scale; i++) {
-            printRow("preMiddle", digits, scale);
-        }
-
-        if  (scale > 0) {
-            printRow("middle", digits, scale);
-        }
-
-        for (int i = 1; i <= scale; i++) {
-            printRow("postMiddle", digits, scale);
-        }
-
-        printRow("bottom", digits, scale);
-
-        o.println();
-    }
-
-    private void printRow(String part, char[] digits, int scale) {
-        if (scale == 0) {
-            return;
-        }
-        for (int i = 0; i < digits.length; i++) {
-            switch (part) {
-                case "top": top(digits[i], scale); break;
-                case "preMiddle": preMiddle(digits[i], scale); break;
-                case "middle": middle(digits[i], scale); break;
-                case "postMiddle": postMiddle(digits[i], scale); break;
-                case "bottom": bottom(digits[i], scale); break;
-            }
-
-            if (i != digits.length-1) {
-                o.print(" ");
+        for (int i = 0; scale > 0 && i < 5; i++) {
+            int times = (i%2 == 0) ? 1 : scale;
+            for (int j = 1; j <= times; j++) {
+                for (byte d = 0; d < digits.length; d++) {
+                    char[] tpl = templates[Character.getNumericValue(digits[d])];
+                    if (i%2 == 0) {
+                        dash(tpl[i], scale);
+                    } else {
+                        pipe(tpl[i], scale);
+                    }
+                    if (d < digits.length - 1) o.print(" ");
+                }
+                o.println();
             }
         }
         o.println();
     }
 
-    private void top(char n, int scale) {
+    private void dash(char format, int scale) {
         o.print(" ");
         for (int i = 1; i <= scale; i++) {
-            if (n == '1' || n == '4') {
+            if (format == 's') {
                 o.print(" ");
             } else {
                 o.print("-");
@@ -84,17 +74,17 @@ public class Main {
         o.print(" ");
     }
 
-    private void preMiddle(char n, int scale) {
-        if (n == '1' || n == '2' || n == '3' || n == '7') {
-            for (int i = 1; i <= 1 + scale; i++) {
+    private void pipe(char format, int scale) {
+        if (format == 'l') {
+            o.print("|");
+            for (int i = 1; i <= scale + 1; i++) {
+                o.print(" ");
+            }
+        } else if (format == 'r') {
+            for (int i = 1; i <= scale + 1; i++) {
                 o.print(" ");
             }
             o.print("|");
-        } else if (n == '5' || n == '6') {
-            o.print("|");
-            for (int i = 1; i <= 1 + scale; i++) {
-                o.print(" ");
-            }
         } else {
             o.print("|");
             for (int i = 1; i <= scale; i++) {
@@ -102,49 +92,5 @@ public class Main {
             }
             o.print("|");
         }
-    }
-
-    private void middle(char n, int scale) {
-        o.print(" ");
-        for (int i = 1; i <= scale; i++) {
-            if (n == '1' || n == '7' || n == '0') {
-                o.print(" ");
-            } else {
-                o.print("-");
-            }
-        }
-        o.print(" ");
-    }
-
-    private void postMiddle(char n, int scale) {
-        if (n == '6' || n == '8' || n == '0') {
-            o.print("|");
-            for (int i = 1; i <= scale; i++) {
-                o.print(" ");
-            }
-            o.print("|");
-        } else if (n == '2') {
-            o.print("|");
-            for (int i = 1; i <= 1 + scale; i++) {
-                o.print(" ");
-            }
-        } else {
-            for (int i = 1; i <= 1 + scale; i++) {
-                o.print(" ");
-            }
-            o.print("|");
-        }
-    }
-
-    private void bottom(char n, int scale) {
-        o.print(" ");
-        for (int i = 1; i <= scale; i++) {
-            if (n == '1' || n == '4' || n == '7') {
-                o.print(" ");
-            } else {
-                o.print("-");
-            }
-        }
-        o.print(" ");
     }
 }
