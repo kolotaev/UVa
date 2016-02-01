9392b6f29b0bb1c22f9538400fa5e227b916ac08
Kolotaev <kolotaev@tut.by>
2016-02-01 19:43:09 +0300

    almost Interpreter

--- sandbox/Main.java
+++ sandbox/Main.java
@@ -2,21 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private int count;
-    private int[] ram = new int[1000];
-    private int[] registers = new int[10];
+    private int count, currentPosition;
+    private int[] ram, registers;
 
     public static void main(String[] args) {
         try {
+//            System.setIn(new FileInputStream(args[0]));
+
             Scanner in = new Scanner(new BufferedInputStream(System.in));
-            Main aMain = new Main();
-            int cycles = in.nextInt();
-            int address;
+            int cycles = Integer.parseInt(in.nextLine());
+            in.nextLine();
             for (int i=0; i < cycles; i++) {
-                address = 0;
-                while (in.hasNextInt()) {
-                    aMain.setRamAt(address++, in.nextInt());
-                }
+                Main aMain = new Main(in);
                 aMain.run();
             }
         } catch (Exception e) {
@@ -26,25 +23,42 @@ public class Main {
         }
     }
 
-    public void setRamAt(int at, int value) {
-        ram[at] = value;
+    public Main(Scanner in) {
+        ram = new int[1000];
+        registers = new int[10];
+        count = currentPosition = 0;
+        String line;
+        for (int i = 0; i < ram.length && in.hasNextLine(); i++) {
+            line = in.nextLine();
+            if (line.equals("")) break;
+            ram[i] = Integer.parseInt(line);
+        }
     }
 
     public void run() {
-        count = 0;
-        for (int a = 0, a < ram.length; a++)
-            execute(ram[a]);
+        while (currentPosition < ram.length) {
+            int stepsToExecute = ram.length - currentPosition;
+            for (int i = currentPosition; i < stepsToExecute; ) {
+                if (i != currentPosition) break;
+                execute(ram[i]);
+                if (currentPosition == i) currentPosition++;
+                i++;
+            }
+        }
+
+        System.out.println(count);
+        System.out.println();
     }
 
     public void execute(int instruction) {
-        count++;
+        if (instruction == 0) return;
+
         int command = instruction / 100;
         int a = instruction / 10 % 10;
-        int b = instruction % 100;
-
+        int b = instruction % 10;
 
         switch (command) {
-            case 1: getInstructionsCount(); break;
+            case 1: halt(); break;
             case 2: setFromValue(a, b); break;
             case 3: addFromValue(a, b); break;
             case 4: multiplyFromValue(a, b); break;
@@ -53,12 +67,14 @@ public class Main {
             case 7: multiplyFromRegister(a, b); break;
             case 8: setFromRAM(a, b); break;
             case 9: setRamFromRegister(a, b); break;
-            case 0: goToRamAddress(a, b); break;
+            case 0: jumpToRamAddress(a, b); break;
         }
+
+        count++;
     }
 
-    public void getInstructionsCount() {
-        System.out.println(count);
+    public void halt() {
+        currentPosition = ram.length + 100;
     }
 
     public void setFromValue(int a, int value) {
@@ -66,11 +82,11 @@ public class Main {
     }
 
     public void addFromValue(int a, int value) {
-        registers[a] += value;
+        registers[a] = ensure(registers[a] + value);
     }
 
     public void multiplyFromValue(int a, int value) {
-        registers[a] *= value;
+        registers[a] = ensure(registers[a] * value);
     }
 
     public void setFromRegister(int a, int b) {
@@ -78,11 +94,11 @@ public class Main {
     }
 
     public void addFromRegister(int a, int b) {
-        registers[a] += registers[b];
+        registers[a] = ensure(registers[a] + registers[b]);
     }
 
     public void multiplyFromRegister(int a, int b) {
-        registers[a] *= registers[b];
+        registers[a] = ensure(registers[a] * registers[b]);
     }
 
     public void setFromRAM(int a, int b) {
@@ -91,11 +107,17 @@ public class Main {
     }
 
     public void setRamFromRegister(int a, int b) {
-        int idx = registers[a];
-        ram[idx] = registers[b];
+        int idx = registers[b];
+        ram[idx] = registers[a];
     }
 
-    public void goToRamAddress(int a, int b) {
+    public void jumpToRamAddress(int a, int b) {
+        if (registers[b] != 0)
+            currentPosition = registers[a];
+    }
 
+    private int ensure(int value) {
+        if (value < 1000) return value;
+        return value % 1000;
     }
 }
