6bebe31c5c7dc78708e23e27247dc9e3d5dd35ed
Egor <kolotaev@tut.by>
2016-02-04 19:23:35 +0300

    almost chess

--- sandbox/Main.java
+++ sandbox/Main.java
@@ -21,16 +21,17 @@ public class Main {
                 emptyRows = 0;
                 for (int i = 0; i < 8; i++) {
                     row = in.nextLine();
+                    if (row.equals("........")) emptyRows++;
                     for (int j = 0; j < 8; j++) {
                         board[i][j] = row.charAt(j);
-                        if (row.equals("........")) emptyRows++;
                     }
                 }
                 in.nextLine();
                 if (emptyRows != 8) {
                     boardsCount++;
+//                    if (boardsCount > 1) System.out.println();
                     aMain.run(board);
-                    System.out.print("Game #" + boardsCount + ": " + aMain.answer + " king is in check.");
+                    System.out.println("Game #" + boardsCount + ": " + aMain.answer + " king is in check.");
                 }
             }
         } catch (Exception e) {
@@ -46,12 +47,12 @@ public class Main {
         for (byte i = 0; i < 8; i++) {
             for (byte j = 0; j < 8; j++) {
                 switch (board[i][j]) {
+                    case 'q': case 'Q' : moveQueen(i, j); break;
+                    case 'r': case 'R' : moveRook(i, j); break;
+                    case 'b': case 'B' : moveBishop(i, j); break;
+                    case 'n': case 'N' : moveKnight(i, j); break;
                     case 'p': case 'P' : movePawn(i, j); break;
                     case 'k': case 'K' : moveKing(i, j); break;
-                    case 'n': case 'N' : moveKnight(figure); break;
-                    case 'b': case 'B' : moveBishop(figure); break;
-                    case 'r': case 'R' : moveRook(figure); break;
-                    case 'q': case 'Q' : moveQueen(figure); break;
                 }
                 if (!answer.equals("no")) return;
             }
@@ -59,13 +60,14 @@ public class Main {
     }
 
     public boolean check(int i, int j, char figure) {
-        if (i >= 0 && i < 8 && j >= 0 && i < 8) {
-            if (Character.isUpperCase(figure) && board[i][j] == 'k')
-                answer = "white";
-            else if (Character.isLowerCase(figure) && board[i][j] == 'K')
-                answer = "black";
-            else if (board[i][j] != 'k' && board[i][j] == 'K' && board[i][j] == '.')
+        if (i >= 0 && i < 8 && j >= 0 && j < 8) {
+            if (board[i][j] != '.' && board[i][j] != 'k' && board[i][j] != 'K')
                 return false;
+
+            if (Character.isUpperCase(figure) && board[i][j] == 'k')
+                answer = "black";
+            else if (Character.isLowerCase(figure) && board[i][j] == 'K')
+                answer = "white";
         }
         return true;
     }
@@ -76,7 +78,7 @@ public class Main {
             check(i+1, j-1, figure);
             check(i+1, j+1, figure);
         } else {
-            check(i+1, j+1, figure);
+            check(i-1, j-1, figure);
             check(i-1, j+1, figure);
         }
     }
@@ -87,6 +89,60 @@ public class Main {
         check(i, j-1, figure);
         check(i+1, j, figure);
         check(i-1, j, figure);
+
+        check(i-1, j-1, figure);
+        check(i-1, j+1, figure);
+        check(i+1, j+1, figure);
+        check(i+1, j-1, figure);
+    }
+
+    public void moveRook(int i, int j) {
+        char figure = board[i][j];
+
+        for (int a = i+1; a < 8; a++)
+            if (!check(a, j, figure)) break;
+
+        for (int a = i-1; a >= 0; a--)
+            if (!check(a, j, figure)) break;
+
+        for (int a = j+1; a < 8; a++)
+            if (!check(i, a, figure)) break;
+
+        for (int a = j-1; a >= 0; a--)
+            if (!check(i, a, figure)) break;
+    }
+
+    void moveBishop(int i, int j) {
+        char figure = board[i][j];
+
+        for (int a = i+1, b = j-1; (a < 8) && (b >= 0); a++, b--)
+            if (!check(a, b, figure)) break;
+
+        for (int a = i+1, b = j+1; (a < 8) && (b < 8); a++, b++)
+            if (!check(a, b, figure)) break;
+
+        for (int a = i-1, b = j-1; (a >= 0) && (b >= 0); a--, b--)
+            if (!check(a, b, figure)) break;
+
+        for (int a = i-1, b = j+1; (a >= 0) && (b < 8); a--, b++)
+            if (!check(a, b, figure)) break;
+    }
+
+    void moveQueen(int i, int j) {
+        moveRook(i, j);
+        moveBishop(i, j);
+    }
+
+    void moveKnight(int i, int j) {
+        char figure = board[i][j];
+        check(i-2, j+1, figure);
+        check(i-2, j-1, figure);
+        check(i+2, j+1, figure);
+        check(i+2, j-1, figure);
+
+        check(i-1, j+2, figure);
+        check(i-1, j-2, figure);
+        check(i+1, j+2, figure);
+        check(i+1, j-2, figure);
     }
 }
-
